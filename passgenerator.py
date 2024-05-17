4164f3e4514008c73b14bda4825333384baf25c2
Georges Machhour <92223746+GeorgesMachhour@users.noreply.github.com>
2024-05-17 18:23:10 +0300

    Create passgenerator.py

---
+++ passgenerator.py
@@ -0,0 +1,20 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "@#$%&*/\?"
+
+Use_for = lower_case + upper_case + number + symbols
+
+length_for_pass = input("Enter the password length (enter for default : 8): ")
+
+if length_for_pass == "":
+    length_for_pass = 8
+else:
+    length_for_pass = int(length_for_pass)
+
+password = "".join(random.sample(Use_for, length_for_pass))
+
+print("Generated password:", password)
+print("Remember to save or write down this password in a secure place.")
